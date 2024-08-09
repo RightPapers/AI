bf742619c33f8b423d93afb36911763b92f68995
leesanghyuk2000 <leesanghyuk2000@gmail.com>
2024-08-09 20:25:33 +0900

    fix : gpt without textrank

--- models/gpt.py
+++ models/gpt.py
@@ -3,7 +3,8 @@ import json
 
 from openai import OpenAI
 from common.keys import my_keys
-from textrank.keysentences import textrank_keysentences
+from common.utils import YouTubeCaptionCrawler
+#from textrank.keysentences import textrank_keysentences
 
 # OpenAI GPT for Video Summarization
 class VS_GPT:
@@ -13,25 +14,26 @@ class VS_GPT:
         self.url = url  # video url
         self.model = model  # default: gpt-3.5-turbo / possible: gpt-4-turbo, gpt-4o-mini
     
-    def _create_prompt(self):
-        # TextRank로 중요 문장 추출
-        summary_data = textrank_keysentences(self.url)
+    #def _create_prompt(self):
+        # # TextRank로 중요 문장 추출
+        # summary_data = textrank_keysentences(self.url)
         
-        prompt = ""
-        for i, line in enumerate(summary_data):
-            sentence_info = f'{i+1}번째 문장의 중요도는 {line["importance"]}이며, 내용은 "{line["sentence"]}"입니다.\n'
-            prompt += sentence_info
+        # prompt = ""
+        # for i, line in enumerate(summary_data):
+        #     sentence_info = f'{i+1}번째 문장의 중요도는 {line["importance"]}이며, 내용은 "{line["sentence"]}"입니다.\n'
+        #     prompt += sentence_info
         
-        return prompt
+        #return prompt
     
     def generate_summary(self):
-        prompt = self._create_prompt()
+        #prompt = self._create_prompt()
+        caption = YouTubeCaptionCrawler(self.url).get_caption()
         completion = self.client.chat.completions.create(
             model=self.model,
             messages=[
-                {"role": "system", "content": "너는 주어진 문장의 중요도와 내용을 바탕으로 비디오를 요약해주는 기능을 가지고 있어."},
-                {"role": "user", "content": "다음 정보를 바탕으로 짜임새 있는 요약문을 작성해줘."},
-                {"role": "user", "content": f"{prompt}"}
+                {"role": "system", "content": "너는 긴 유튜브 영상을 5개의 문장으로 요약하는 모델이야."},
+                {"role": "user", "content": "다음 내용을 요약문으로 작성해줘."},
+                {"role": "user", "content": f"{caption}"}
             ]
         )
         
