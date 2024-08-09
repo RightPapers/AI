84827efa05db17dd34870e3da664385782c29327
leesanghyuk2000 <leesanghyuk2000@gmail.com>
2024-08-09 19:43:41 +0900

    fix : libraries, add textrank

--- models/gpt.py
+++ models/gpt.py
@@ -1,22 +1,24 @@
 # coding : utf-8
 import json
+
 from openai import OpenAI
 from common.keys import my_keys
+from textrank.keysentences import textrank_keysentences
 
 # OpenAI GPT for Video Summarization
 class VS_GPT:
-    def __init__(self, file_path, model='gpt-3.5-turbo'):
+    def __init__(self, url, model='gpt-3.5-turbo'):
         self.api_key = my_keys('openai')
         self.client = OpenAI(api_key=self.api_key)
-        self.file_path = file_path  # json file_path
+        self.url = url  # video url
         self.model = model  # default: gpt-3.5-turbo / possible: gpt-4-turbo, gpt-4o-mini
     
     def _create_prompt(self):
-        with open(self.file_path, 'r', encoding='utf-8') as f:
-            data = json.load(f)
+        # TextRank로 중요 문장 추출
+        summary_data = textrank_keysentences(self.url)
         
         prompt = ""
-        for i, line in enumerate(data):
+        for i, line in enumerate(summary_data):
             sentence_info = f'{i+1}번째 문장의 중요도는 {line["importance"]}이며, 내용은 "{line["sentence"]}"입니다.\n'
             prompt += sentence_info
         
