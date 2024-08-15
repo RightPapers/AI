5e809254ea073ddaa4197451293f6df166f3612a
leesanghyuk2000 <leesanghyuk2000@gmail.com>
2024-08-15 23:22:40 +0200

    fix : new prompt

--- models/gpt.py
+++ models/gpt.py
@@ -31,7 +31,7 @@ class VS_GPT:
         completion = self.client.chat.completions.create(
             model=self.model,
             messages=[
-                {"role": "system", "content": "너는 긴 유튜브 영상을 요약하는 모델이야. 5문장으로 요약하고, 첫번째 문장은 영상 전체를 설명하는 핵심 단어로 짧게 구성해줘. 4문장은 영상의 상세 내용을 요약해줘"},
+                {"role": "system", "content": "너는 긴 유튜브 영상을 요약하는 모델이야. 5문장으로 요약하고, 첫번째 문장은 영상 전체를 핵심을 담아 짧게 구성해줘. 4문장은 영상의 상세 내용을 문장 형태로 요약해줘"},
                 {"role": "user", "content": "다음 내용을 요약문으로 작성해줘."},
                 {"role": "user", "content": f"{caption}"}
             ]
