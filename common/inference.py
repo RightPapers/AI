2778d1c57699096a606a6c56778e71e32aa81a4a
leesanghyuk2000 <leesanghyuk2000@gmail.com>
2024-09-03 21:11:49 +0900

    fix : delete duplicated lines

--- common/inference.py
+++ common/inference.py
@@ -31,13 +31,13 @@ def model_inference(url, transformer=True):
         model_path = './models/transformer_ACC_0.9231.pth'
         text_config = AutoConfig.from_pretrained(model_checkpoint) # 모델 설정
         model = BD_Transformer(text_config) # 모델
-        model.load_state_dict(torch.load(model_path, map_location=device)) # 파라미터 덮어쓰기
+        
     else:        
         model_checkpoint = 'klue/roberta-large'
         model_path = './models/roberta_ACC_0.9265.pth'
         model = BD_Roberta(model_checkpoint) # 모델
-        model.load_state_dict(torch.load(model_path, map_location=device)) # 파라미터 덮어쓰기
-        
+    
+    model.load_state_dict(torch.load(model_path, map_location=device)) # 파라미터 덮어쓰기
     model.to(device)
     
     tokenizer = AutoTokenizer.from_pretrained(model_checkpoint) # 토크나이저
