1fb7561ecd4a9ccb084cf41d767828727dc8968f
leesanghyuk2000 <leesanghyuk2000@gmail.com>
2024-09-23 02:14:20 +0900

    Add LeNet layer

--- common/layers.py
+++ common/layers.py
@@ -137,3 +137,30 @@ class AttentionMechanism(nn.Module):
         attention_weights = F.softmax(attention_weights, dim=1)
         context_vector = torch.sum(features * attention_weights.unsqueeze(-1), dim=1)
         return context_vector, attention_weights
+
+# LeNet
+class LeNet(nn.Module):
+    def __init__(self, output_dim=768):
+        super(LeNet, self).__init__()
+        self.cnn1 = nn.Conv2d(in_channels=3, out_channels=16, kernel_size=5, stride=1, padding=0)
+        self.relu1 = nn.ReLU()
+        self.maxpool1 = nn.MaxPool2d(kernel_size=2)
+        self.cnn2 = nn.Conv2d(in_channels=16, out_channels=32, kernel_size=5, stride=1, padding=0)
+        self.relu2 = nn.ReLU()
+        self.maxpool2 = nn.MaxPool2d(kernel_size=2)
+        self.adaptive_pool = nn.AdaptiveAvgPool2d((7, 7)) # 고정된 크기의 출력, Linear를 사용하면 이미지 크기에 따라 바뀜
+        self.fc1 = nn.Linear(32 * 7 * 7, output_dim) # RoBERTa의 출력크기인 768과 맞춰줌
+        self.relu3 = nn.ReLU()
+
+    def forward(self, x):
+        out = self.cnn1(x)
+        out = self.relu1(out)
+        out = self.maxpool1(out)
+        out = self.cnn2(out)
+        out = self.relu2(out)
+        out = self.maxpool2(out)
+        out = self.adaptive_pool(out)
+        out = out.view(out.size(0), -1)  # Flatten
+        out = self.fc1(out)
+        out = self.relu3(out)
+        return out
